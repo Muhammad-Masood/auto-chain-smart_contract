011a5d2cd0603d078cb527e4984cf8212c624a4f
MUHAMMAD MASOOD <sp22bsse0037@maju.edu.pk>
2024-01-02 11:37:35 +0000

    Update 3 files

--- src/AutoChain.sol
+++ src/AutoChain.sol
@@ -7,16 +7,18 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract AutoChain is ERC721, Ownable {
     uint256 private totalSupply;
     mapping(uint256 tokenId => string[]) private carToMaintenanceHistory;
+    mapping(uint256 tokenId => string) private tokenIdToURI;
 
     constructor() ERC721("AutoChain", "AC") Ownable(msg.sender) {}
 
     /**
      * launch new car or mint new token
-     * @param _to user address at which the nft should be minted
+     * @param _tokenURI uri of the token to mint
      */
 
-    function mint() external onlyOwner {
+    function mint(string memory _tokenURI) external onlyOwner {
         _safeMint(msg.sender, totalSupply);
+        tokenIdToURI[totalSupply] = _tokenURI;
         totalSupply+=1;
     }
 
@@ -28,6 +30,7 @@ contract AutoChain is ERC721, Ownable {
 
     function burn(uint256 _tokenId) external onlyOwner {
         _burn(_tokenId);
+        tokenIdToURI[_tokenId] = "";
     }
 
     function updateMaintenanceHistory(uint256 _tokenId, string memory _update) external {
@@ -46,8 +49,7 @@ contract AutoChain is ERC721, Ownable {
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         _requireOwned(tokenId);
-        string memory baseURI = _baseURI();
-        return bytes(baseURI).length > 0 ? string.concat(baseURI, tokenId.toString()) : "";
+        return tokenIdToURI[tokenId];
     }
 
     // internal functions
--- src/AutoChainRental.sol
+++ src/AutoChainRental.sol
@@ -12,24 +12,26 @@ contract AutoChainRental is AutoChain {
         uint256 durationFrom;
         uint256 durationTill;
         string terms;
+        bool isRented;
     }
 
     mapping(uint256 tokenId => RentalDetails) private tokenIdToRentalDetails;
 
     // Events
-
     event (uint256 indexed _tokenId, address _renter) CarRented;
 
     constructor() {}
 
+    // list car for rent
     function listCar(uint256 _tokenId, RentalDetails memory _rentalDetails) external {
         tokenIdToRentalDetails[_tokenId] = _rentalDetails;
-        
     }
 
+    // rent the listed car
     function rentCar(uint256 _tokenId) external {
         address owner = _requireOwned(_tokenId);
         // transfer
+        tokenIdToRentalDetails[_tokenId].isRented = true;
         emit CarRented(_tokenId, msg.sender);
     }
 
