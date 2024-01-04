882a22e1ea999b3aa25ef7b868e9f25c4a054a3c
MUHAMMAD MASOOD <sp22bsse0037@maju.edu.pk>
2024-01-04 13:51:56 +0000

    Update 3 files

--- script/AutoChainNFTDeploy.s.sol
+++ script/AutoChainNFTDeploy.s.sol
@@ -5,7 +5,6 @@ import {Script, console} from "forge-std/Script.sol";
 import {AutoChain} from "../src/AutoChainNFT.sol";
 
 contract AutoChainNFTDeployScript is Script {
-    address public owner = vm.addr(123);
     
     function run() external returns(AutoChain) {
         vm.startBroadcast();
--- test/AutoChainTest.t.sol
+++ test/AutoChainTest.t.sol
@@ -7,28 +7,21 @@ import {AutoChainNFTDeployScript} from "../script/AutoChainNFTDeploy.s.sol";
 
 contract AutoChainTest is Test {
     AutoChain public autoChain;
-    address public deployer = vm.addr(123);
     address public user = vm.addr(2233);
+    address public owner;
 
     error OwnableUnauthorizedAccount(address account);
 
     function setUp() public {
-        vm.startPrank(deployer);
         AutoChainNFTDeployScript autoChainScript = new AutoChainNFTDeployScript();
         autoChain = autoChainScript.run();
-        vm.stopPrank();
-        console.log(user);
-        console.log(deployer);
+        owner = autoChain.owner();
     }
 
     modifier mintToken(){
-        vm.prank(deployer);
+        vm.prank(owner);
         autoChain.mint("new_token_uri");
         _;
-    }
-
-    function test_DeployerShouldBeOwner() public {
-        assertEq(autoChain.owner(),deployer);
     }
 
     function testFail_IfMinterIsNotOwner() public {
@@ -36,4 +29,6 @@ contract AutoChainTest is Test {
         vm.prank(user);
         autoChain.mint("token_1_uri");
     }
+
+    
 }
