b04f599071d95161e1f4c3682ac8dd95a5f55f74
MUHAMMAD MASOOD <sp22bsse0037@maju.edu.pk>
2024-01-04 13:05:07 +0000

    Update 3 files

--- script/AutoChainNFTDeploy.s.sol
+++ script/AutoChainNFTDeploy.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.22;
 
 import {Script, console} from "forge-std/Script.sol";
-import {AutoChain} from "../src/AutoChain.sol";
+import {AutoChain} from "../src/AutoChainNFT.sol";
 
-contract AutoChainDeployScript is Script {
+contract AutoChainNFTDeployScript is Script {
     address public owner = vm.addr(123);
     
     function run() external returns(AutoChain) {
         vm.startBroadcast();
-        vm.prank(owner);
         AutoChain autoChain = new AutoChain();
-        console.log("Deployed AutoChain.sol on: ", address(autoChain), "owner: ", owner);
         vm.stopBroadcast();
+        console.log("Deployed AutoChain.sol on: ", address(autoChain), "owner: ", autoChain.owner());
+        console.log(address(this));
         return autoChain;
     }
 }
